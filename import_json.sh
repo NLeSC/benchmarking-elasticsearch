76238af3829759aff6169f2643e8c04413382844
Lars Buitinck <l.buitinck@esciencecenter.nl>
2015-10-02 22:38:23 +0200

    get rid of tempfile in import_json

--- import_json.sh
+++ import_json.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-for f in *.gz;
-do
-  echo "Processing $f"
-  gunzip -c $f > requests
-  curl -s -XPOST localhost:9200/kb/doc/_bulk --data-binary @requests > /dev/null
+#!/bin/sh
+for f in "$1"/*.gz; do
+  echo "Importing $f"
+  gunzip -c $f |
+    curl -s -XPOST localhost:9200/kb_20140605/doc/_bulk --data-binary @- > /dev/null
 done
